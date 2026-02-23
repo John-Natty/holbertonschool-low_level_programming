502b4ad0852f1dc6467ec82505b969bfcfca78a1
John-Natty <jonathanmerle8426@gmail.com>
2026-02-23 13:11:45 +0100

    spacev2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Description: prints a sentence using puts
  * Return: Always 0(Success)
  */
-int main (void)
+int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
     return (0);
