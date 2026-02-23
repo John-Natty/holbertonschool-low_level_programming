20d2d056a39e8e27847e6101987d50726157b710
John-Natty <jonathanmerle8426@gmail.com>
2026-02-23 13:06:18 +0100

    Task4.Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * *main - Entry point
+ * 
+ * Description: prints a sentence using puts
+ * Return: Always 0(Success)
+ */
+int main (void)
+{
+    puts("Programming is like building a multilingualpuzzle");
+    return (0);
+}
