6664417252bf0a5a7ea7bd11bff2e3ca43b75aa0
John-Natty <jonathanmerle8426@gmail.com>
2026-03-04 11:14:11 +0100

    review the task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,22 @@
 #include "main.h"
+
 /**
-* _strcmp - compares two strings
-* @s1: pointer to first string
-* @s2: pointer to second string
-*
-* Return: 0 if strings are equal, negative value if s1
-*< s2, positive value if s1 > s2
-*/
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: difference between the first different characters
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	i = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		i++;
 	}
-	return (0);
+
+	return (s1[i] - s2[i]);
 }
