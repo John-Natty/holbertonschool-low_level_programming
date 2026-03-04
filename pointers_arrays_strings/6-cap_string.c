358cabaff5363528693be0846eaecb0ac8920aaa
John-Natty <jonathanmerle8426@gmail.com>
2026-03-04 11:43:29 +0100

    Task6.Expect the best. Prepare for the worst. Capitalize on what comes: Write a function that capitalizes all words of a string.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: pointer to the string to be modified
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - ('a' - 'A');
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (i > 0 && str[i - 1] == sep[j] &&
+			    str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - ('a' - 'A');
+		}
+	}
+
+	return (str);
+}
