aed35374402022382c07f3caa88efce0784123c2
John-Natty <jonathanmerle8426@gmail.com>
2026-03-04 11:48:08 +0100

    Task7.Mozart composed his music not for the elite, but for everybody

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @str: pointer to the string to be modified
+ *
+ * Return: pointer to the modified string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char leet[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leet[j] != '\0'; j++)
+		{
+			if (str[i] == leet[j])
+			{
+				str[i] = num[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
