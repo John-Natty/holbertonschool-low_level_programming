82d876ed0df71f1100954faf4249ef801296f3c1
John-Natty <jonathanmerle8426@gmail.com>
2026-03-04 11:10:58 +0100

    Task3.strcmp: Write a function that compares two strings.

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,8 @@
 * @s1: pointer to first string
 * @s2: pointer to second string
 *
-* Return: 0 if strings are equal, negative value if s1 < s2, positive value if s1 > s2
+* Return: 0 if strings are equal, negative value if s1
+*< s2, positive value if s1 > s2
 */
 int _strcmp(char *s1, char *s2)
 {
