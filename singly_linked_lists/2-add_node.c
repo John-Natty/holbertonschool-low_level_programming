924cac3de6f35132d008505745ec4438bf292d08
John-Natty <jonathanmerle8426@gmail.com>
2026-03-26 13:35:20 +0100

    task 2 correction

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len;
 
 	if (head == NULL)
 		return (NULL);
@@ -32,7 +33,11 @@ list_t *add_node(list_t **head, const char *str)
 			free(new_node);
 			return (NULL);
 		}
-		new_node->len = strlen(str);
+		len = 0;
+		while (str[len] != '\0')
+			len++;
+
+		new_node->len = len;
 	}
 
 	new_node->next = *head;
