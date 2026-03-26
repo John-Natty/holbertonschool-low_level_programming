a5cb4a9e0970cf12d830df9365c495b5ab1bc9c7
John-Natty <jonathanmerle8426@gmail.com>
2026-03-26 13:16:59 +0100

    correction task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
- * Return: the number of nodes in the list
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
